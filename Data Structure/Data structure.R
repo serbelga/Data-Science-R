118dc7f2faa3799ea29917ea45c5f54aef328ffa
Sergio Belda <serbelga@inf.upv.es>
2018-12-13 11:38:27 +0100

    Added Data Structure folder

---
+++ Data Structure/Data structure.R
@@ -0,0 +1,26 @@
+list <- 1:12
+
+v <- c("a", "b")
+l <- c(3, 2, 1, 4)
+
+max(l)
+
+min(l)
+
+range(l)
+
+length(l)
+
+prod(l)
+
+sum(l)
+
+median(l)
+
+var(l)
+
+mean(l)
+
+sort(l)
+
+seq (1, 5, 0.5)
