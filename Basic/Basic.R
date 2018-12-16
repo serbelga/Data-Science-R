761066eebd27c228599762adc5a3d4546935cb7a
Sergio Belda <serbelga@inf.upv.es>
2018-12-16 03:15:15 +0100

    Basic R

--- Basic/Basic.R
+++ Basic/Basic.R
@@ -1,4 +1,26 @@
+# Assign
 1->a
 a<-1
 a=1
-assign("a",1)
+assign("a", 1)
+
+# Type (double, character, logical, ...)
+typeof(a)
+
+# Mode (numeric, logical, character)
+mode(a)
+
+# Length
+length(a)
+
+# Sum
+s<-1+1
+
+# Substraction
+r<-2-1
+
+# Product
+p<-1*2
+
+# Division
+d<-4/2
