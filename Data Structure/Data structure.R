4c6aaf41645dccfdb72d53e53fcd42360791ec37
Sergio Belda <serbelga@inf.upv.es>
2018-12-16 14:16:50 +0100

    Update Data structure.R

--- Data Structure/Data structure.R
+++ Data Structure/Data structure.R
@@ -1,48 +1,53 @@
+# List
 list <- 1:12
 
+# Vector
 v <- c("a", "b")
 l <- c(3, 2, 1, 4)
 
+# Maximum
 max(l)
 
+# Minimum
 min(l)
 
+# Range
 range(l)
 
+# Length
 length(l)
 
+# Product
 prod(l)
 
+# Sum
 sum(l)
 
+# Median
 median(l)
 
+# Variance
 var(l)
 
+# Mean
 mean(l)
 
+# Sort
 sort(l)
 
+# Sequence
 seq (1, 5, 0.5)
 
+# Repetition
 rep(c(6,2,4),4)
 
+# Matrix
 matrix(1:15, 5, 3, byrow = TRUE)
-     [,1] [,2] [,3]
-[1,]    1    2    3
-[2,]    4    5    6
-[3,]    7    8    9
-[4,]   10   11   12
-[5,]   13   14   15
 
 rep(1:2, each = 2)
-[1] 1 1 2 2
 rep(c(1, 4), each = 2)
-[1] 1 1 4 4
 rep(1:4, c(2,1,2,1))
-[1] 1 1 2 3 3 4
 rep(c(9, 2, 5), c(6, 5, 4))
-[1] 9 9 9 9 9 9 2 2 2 2 2 5 5 5 5
 
 
 column_names<-c("height", "shoesize", "gender", "population")
