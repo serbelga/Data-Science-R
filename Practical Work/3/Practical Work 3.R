406ae6ad85cf7a1fb5b97f9c31b7e1a42febea57
Sergio Belda <serbelga@inf.upv.es>
2018-12-22 15:17:38 +0100

    Update Practical Work 3.R

--- Practical Work/3/Practical Work 3.R
+++ Practical Work/3/Practical Work 3.R
@@ -28,3 +28,16 @@ xdate[which(xdate$meanAvailable == max(xdate$meanAvailable)),]
 #min and max values
 
 # What is the month with the least bikes available (in average)?
+
+month(dat$date)
+month<-month(dat$date)
+dat$month<-month
+xmonth <- group_by(dat, month)
+xmonth <- summarise(xmonth, meanAvailable = mean(avg_available))
+ggplot(data=xmonth,aes(x=month, y=meanAvailable, group=month, colour = factor(month)))+
+  geom_area()+
+  theme_bw()+
+  scale_colour_discrete("month", labels=c("January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))+
+  ggtitle("Guillem de Castro. Month Average")+
+  labs(x ="Month", y ="Available Bicycles")+
+  theme(plot.title = element_text(face="bold", size=18, hjust = 0.5))
