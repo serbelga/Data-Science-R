66762aefd8d593faeeee43aa43a07c9a45b56b20
Sergio Belda <serbelga@inf.upv.es>
2019-01-10 16:45:55 +0100

    Update Practical Work 3.R

--- Practical Work/3/Practical Work 3.R
+++ Practical Work/3/Practical Work 3.R
@@ -1,3 +1,6 @@
+setwd('/Users/sergiobelda/Desktop/dasresources/')
+dat<-read.csv('valenbisi_2011_2014.csv', sep=";")
+
 # What is the day of the week with the least bikes available (in average)?
 dat$date <- ymd(dat$date)
 dayweek <- wday(dat$date)
@@ -41,3 +44,38 @@ ggplot(data=xmonth,aes(x=month, y=meanAvailable, group=month, colour = factor(mo
   ggtitle("Guillem de Castro. Month Average")+
   labs(x ="Month", y ="Available Bicycles")+
   theme(plot.title = element_text(face="bold", size=18, hjust = 0.5))
+
+# Does the use of the station change in summer? Consider that summer is the period from July to September (both monts included)
+
+month(dat$date)
+month<-month(dat$date)
+dat$month<-month
+dat %>% mutate(month = month) %>%
+  group_by(group = if_else(month < 7 | month > 9,
+                           'no_verano',
+                           'verano'))->season
+
+summarise(season, meanAvailable = mean(avg_available))
+
+# Result:
+# A tibble: 2 x 2
+# group     meanAvailable
+# <chr>             <dbl>
+# 1 no_verano          4.14
+# 2 verano             4.45
+
+# Do the Fallas affect the use of the station? Fallas take place in Valencia from 15 to 19 March.
+
+dat %>% mutate(date = dat$date) %>%
+  group_by(group = if_else((day(date)>=15 & day(date)<=19) & month(date) == 3,
+                           'fallas',
+                           'no_fallas'))->fallas
+
+summarise(fallas, meanAvailable = mean(avg_available))
+
+# Result:
+# A tibble: 2 x 2
+# group     meanAvailable
+# <chr>             <dbl>
+# 1 fallas             0   
+# 2 no_fallas          4.26
